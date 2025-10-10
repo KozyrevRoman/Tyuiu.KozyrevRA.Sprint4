8dc2414e2b249fa1e2decc2cc85b64cc4caa06b8
Роман Козырев <Tyuiu.KozyrevRA@outlook.com>
2025-10-10 19:32:11 +0500

    Task1

---
+++ Tyuiu.KozyrevRA.Sprint4.Task1.V14.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.KozyrevRA.Sprint4.Task1.V14.Lib;
+namespace Tyuiu.KozyrevRA.Sprint4.Task1.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        { 
+            DataService ds = new DataService();
+            int[] list = { 1, 5, 3, 6, 5, 4, 8, 6, 8, 4, 2, 3, 1 };
+            int res = ds.Calculate(list);
+            int wait = 18;
+            Assert.AreEqual(res, wait);
+
+        }
+    }
+}
---
+++ Tyuiu.KozyrevRA.Sprint4.Task1.V14/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KozyrevRA.Sprint4.Task1.V14.Lib;
+namespace Tyuiu.KozyrevRA.Sprint4.Task1.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Козырев Р. А. | СМАРТБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема:  Одномерные массивы. (ввод с клавиатуры)                          *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнил: Козырев Роман Артёмович | СМАРТБ-25-1                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 13 элементов заполненный         *");
+            Console.WriteLine("* статическими значениями в диапазоне от 1 до 8                           *");
+            Console.WriteLine("* Подсчитать сумму нечетных элементов массива.                            *");
+            Console.WriteLine("* С клавиатуры: 1, 5, 3, 6, 5, 4, 8, 6, 8, 4, 2, 3, 1                     *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.Write("Введите длину массива: ");
+            int len = Convert.ToInt32(Console.ReadLine());
+
+            int[] mas = new int[len];
+
+            for (int i = 0; i < len; i++)
+            {
+                Console.Write($"Введите элемент {i}: ");
+                mas[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(mas));
+            Console.ReadKey();
+        }
+    }
+}
