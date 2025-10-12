dd52c0f9507425c40ee30bce89167bdd912ff74d
Роман Козырев <Tyuiu.KozyrevRA@outlook.com>
2025-10-12 19:28:24 +0500

    Task5

---
+++ Tyuiu.KozyrevRA.Sprint4.Task5.V1.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.KozyrevRA.Sprint4.Task5.V1.Lib;
+namespace Tyuiu.KozyrevRA.Sprint4.Task5.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            int[,] inputMas =
+            {
+                { 3, 4, -4, 6, 4 },
+                { 6, 4, 6, 4, 5 },
+                { 5, 4, 4, 4, 0 },
+                { 3, 5, 6, 4, 6 }
+            };
+
+            DataService ds = new DataService();
+            int[,] res = ds.Calculate(inputMas);
+            int[,] wait =
+            {
+                { 1, 1, -4, 1, 1 },
+                { 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 1 }
+            }; 
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KozyrevRA.Sprint4.Task5.V1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KozyrevRA.Sprint4.Task5.V1.Lib;
+namespace Tyuiu.KozyrevRA.Sprint4.Task5.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Козырев Р. А. | СМАРТБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема:  Двумерные массивы. (генератор случайных чисел)                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Козырев Роман Артёмович | СМАРТБ-25-1                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* случайными значениями в диапазоне от -9 до 8.                           *");
+            Console.WriteLine("* Заменить положительные элементы на 1.                                   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int[,] mas = new int[5, 5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    mas[i, j] = rnd.Next(-9, 8);
+                }
+            }
+
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                for (int j = 0; j < mas.GetLength(1); j++)
+                {
+                    Console.Write(mas[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int[,] resMas = ds.Calculate(mas);
+
+            for (int i = 0; i < resMas.GetLength(0); i++)
+            {
+                for (int j = 0; j < resMas.GetLength(1); j++)
+                {
+                    Console.Write(resMas[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
